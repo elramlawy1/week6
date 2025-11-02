2d5d4589eefb04202f8ed5af8fd4262237ac697d
elramlawy1 <yousef.alramlawy.2020@gmail.com>
2025-11-03 00:45:41 +0100

    Complete Java 8 Stream API exercises 1-9

---
+++ src/test/java/stream/api/Exercise9Test.java
@@ -0,0 +1,96 @@
+package stream.api;
+
+import common.test.tool.annotation.Difficult;
+import common.test.tool.annotation.Easy;
+import common.test.tool.dataset.ClassicOnlineStore;
+import common.test.tool.entity.Customer;
+import common.test.tool.entity.Item;
+import common.test.tool.util.CollectorImpl;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class Exercise9Test extends ClassicOnlineStore {
+
+    @Easy
+    @Test
+    public void simplestStringJoin() {
+        List<Customer> customerList = this.mall.getCustomerList();
+
+        /**
+         * Implement a {@link Collector} which can create a String with comma separated names shown in the assertion.
+         * The collector will be used by serial stream.
+         */
+        Supplier<Object> supplier = () -> new StringJoiner(",");
+        Collector<String, StringJoiner, String> toCsv = Collector.of(
+                () -> new StringJoiner(","),
+                (joiner, name) -> joiner.add(name),
+                (joiner1, joiner2) -> joiner1.merge(joiner2),
+                joiner -> joiner.toString()
+        );
+        String nameAsCsv = customerList.stream().map(Customer::getName).collect(toCsv);
+        assertThat(nameAsCsv, is("Joe,Steven,Patrick,Diana,Chris,Kathy,Alice,Andrew,Martin,Amy"));
+    }
+
+    @Difficult
+    @Test
+    public void mapKeyedByItems() {
+        List<Customer> customerList = this.mall.getCustomerList();
+
+        /**
+         * Implement a {@link Collector} which can create a {@link Map} with keys as item and
+         * values as {@link Set} of customers who are wanting to buy that item.
+         * The collector will be used by parallel stream.
+         */
+
+
+
+        Collector<Customer, Map<String, Set<String>>, Map<String, Set<String>>> toItemAsKey = Collector.of(
+                HashMap::new,
+                (map, customer) -> {
+                    customer.getWantToBuy().forEach(item -> {
+                        map.computeIfAbsent(item.getName(), k -> new HashSet<>()).add(customer.getName());
+                    });
+                },
+                (map1, map2) -> {
+                    map2.forEach((item, customers) -> {
+                        map1.computeIfAbsent(item, k -> new HashSet<>()).addAll(customers);
+                    });
+                    return map1;
+                }
+        );
+        Map<String, Set<String>> itemMap =
+                customerList.stream().parallel().collect(toItemAsKey);
+
+
+        assertThat(itemMap.get("plane"), containsInAnyOrder("Chris"));
+        assertThat(itemMap.get("onion"), containsInAnyOrder("Patrick", "Amy"));
+        assertThat(itemMap.get("ice cream"), containsInAnyOrder("Patrick", "Steven"));
+        assertThat(itemMap.get("earphone"), containsInAnyOrder("Steven"));
+        assertThat(itemMap.get("plate"), containsInAnyOrder("Joe", "Martin"));
+        assertThat(itemMap.get("fork"), containsInAnyOrder("Joe", "Martin"));
+        assertThat(itemMap.get("cable"), containsInAnyOrder("Diana", "Steven"));
+        assertThat(itemMap.get("desk"), containsInAnyOrder("Alice"));
+    }
+
+}
